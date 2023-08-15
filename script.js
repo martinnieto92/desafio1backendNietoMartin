461ccffd2aa8dd18fd1d834c8c67c076865f280f
martinnieto92 <nietomartin92@gmail.com>
2023-08-14 23:16:53 -0300

    desafio2

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-class ProductManager{
-    constructor(){
-        this.Products=[]
-    }
-    addProduct(title, description, price, thumbnail, code, stock){
+const fs = require('fs')
+
+class ProductManager {
+    constructor(filePath) {
+        this.Products = [];
+        this.path = filePath;
         
+    }
+
+     // Guardar productos en el archivo
+    saveProducts() {
+        fs.writeFileSync(this.path, JSON.stringify(this.Products, null, '\t'));
+    }
+
+    // Actualizar un campo específico de un producto por su ID
+    updateProduct(id, field, value) {
+        const productToUpdate = this.Products.find(product => product.id === id);
+        if (!productToUpdate) {
+            console.log(`Error: No se encontró ningún producto con el ID ${id}.`);
+            return;
+        }
+        productToUpdate[field] = value; // Actualizar el campo especificado
+        this.saveProducts(); // Guardar cambios después de actualizar
+    }
+
+     // Eliminar un producto por su ID
+    deleteProduct(id) {
+        const indexToDelete = this.Products.findIndex(product => product.id === id);
+        if (indexToDelete === -1) {
+            console.log(`Error: No se encontró ningún producto con el ID ${id}.`);
+            return;
+        }
+
+        this.Products.splice(indexToDelete, 1); // Eliminar el producto de la lista
+        this.saveProducts(); // Guardar cambios después de eliminar
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+
         const productExists = this.Products.some(
             product => product.title === title && product.code === code
         );
-        
+
         if (productExists) {
-            throw new Error(`Error: Ya existe un producto con el mismo título y código.`);
+            console.log((`Error: Ya existe un producto con el mismo título y código.`));
         }
-        
+
         let nuevoProducto = {
             title,
             description,
@@ -20,43 +53,51 @@ class ProductManager{
             code,
             stock
         }
-        if (this.Products.length===0){
-            nuevoProducto.id=1
+        if (this.Products.length === 0) {
+            nuevoProducto.id = 1
         }
         else
-        nuevoProducto.id=this.Products.length+1
+            nuevoProducto.id = this.Products.length + 1
 
         this.Products.push(nuevoProducto)
     }
 
-    getProducts(){
+    getProducts() {
         return this.Products
     }
 
     getProductById(id) {
         const product = this.Products.find(product => product.id === id);
         if (!product) {
-            throw new Error(`Error: No se encontró ningún producto con el ID ${id}.`);
+            console.log((`Error: No se encontró ningún producto con el ID ${id}.`));
         }
         return product;
     }
 
 }
 
-let tm = new ProductManager();
+let path = './archivos/textoSincrono.json'
+
+
+let tm = new ProductManager(path);
 console.log(tm.getProducts());
 
-try {
-    tm.addProduct('mesa', 'cuadrada', 500, 'foto', 123, 500);
-    tm.addProduct('mate', 'redondo', 200, 'foto', 321, 300);
-    tm.addProduct('notebook', 'roja', 9500, 'foto', 323, 100);
-    tm.addProduct('notebook', 'roja', 9500, 'foto', 323, 100);
-    console.log(tm.getProducts());
 
-    const product = tm.getProductById(2);
+tm.addProduct('mesa', 'cuadrada', 500, 'foto', 123, 500);
+tm.addProduct('mate', 'redondo', 200, 'foto', 321, 300);
+tm.addProduct('notebook', 'roja', 9500, 'foto', 323, 100);
+//tm.addProduct('notebook', 'roja', 9500, 'foto', 323, 100);
+console.log(tm.getProducts()); //arreglo vacio
+
+const product = tm.getProductById(3);
+
+if (product !== undefined) {
     console.log("Producto encontrado:", product);
-    
-    const nonExistentProduct = tm.getProductById(2); 
-} catch (error) {
-    console.error(error.message);
+    fs.writeFileSync(path, JSON.stringify(product, null, '\t'))
 }
+
+tm.updateProduct(2, 'price', 250); // Actualizar el precio del producto con ID 2,, en este caso solo actualizo una variable
+console.log(tm)
+
+tm.deleteProduct(1); // Eliminar el producto con ID 1
+console.log(tm)
